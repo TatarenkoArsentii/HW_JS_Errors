902d51df5f895dafe1b5f27b71a00ad918cf8ba5
Arsentii <tatarenkoarsen@gmail.com>
2021-10-11 15:12:52 +0300

    initial

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const body = document.body,
+  div = body.querySelector(".div"),
+  li = body.querySelectorAll("li"),
+  inputR = body.querySelector('input[name="red"]'),
+  inputG = body.querySelector('input[name="green"]'),
+  inputB = body.querySelector('input[name="blue"]'),
+  inputA = body.querySelector('input[name="alpha"]'),
+  optionValue = body.querySelector('select[name="option"]'),
+  saveBtn = body.querySelector("#save");
+console.log(li);
+
+class RGBA{
+    
+}
